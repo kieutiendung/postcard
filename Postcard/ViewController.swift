2f5557bf267a12ecc056af52bcc7819d673403a5
Kieu Dung <kieudung@Kieus-MacBook-Pro.local>
2014-10-20 20:08:11 +0700

    first commit using commonline

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelMessage: UILabel!
+    @IBOutlet weak var enterName: UITextField!
+    @IBOutlet weak var enterMessage: UITextField!
 
+    @IBOutlet weak var buttonchange: UIButton!
+    @IBOutlet var myView: UIView!
+    @IBAction func sendMessage(sender: UIButton) {
+        labelMessage.hidden = false;
+        //labelMessage.text   = "Hello Worlk";
+        labelMessage.text = enterMessage.text;
+        enterMessage.text = "";
+        //enterMessage.resignFirstResponder();
+        myView.endEditing(true);
+        buttonchange.setTitle("button press", forState: UIControlState());
+        buttonchange.setTitleColor(UIColor.redColor(), forState: UIControlState());
+        buttonchange.backgroundColor = UIColor.blueColor();
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
